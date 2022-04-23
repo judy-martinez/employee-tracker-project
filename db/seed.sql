0c489e5e4ffccb480c9852ec2310c75b3eb9f70d
Judy <judy.mtz1@gmail.com>
2022-04-23 03:33:38 -0500

    took out id from employee seeds to have less to populate

--- db/seed.sql
+++ db/seed.sql
@@ -4,5 +4,5 @@ VALUES
 INSERT INTO roles (title, salary, department_id, manager)
 VALUES
 
-INSERT INTO employee (id, first_name, last_name, roles_id, manager_id)
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
 VALUES
