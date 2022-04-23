b0eb4734c3717cf5ee500ba25f47b84f6bd0dfda
Judy <judy.mtz1@gmail.com>
2022-04-23 03:07:54 -0500

    started seeds

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO deparment(name)
+VALUES
+
+INSERT INTO roles (title, salary, department_id, manager)
+VALUES
+
+INSERT INTO employee (id, first_name, last_name, roles_id, manager_id)
+VALUES
