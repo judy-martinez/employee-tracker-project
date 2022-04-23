db5cb75a6f07ab39e91db3e41b600a6da812d360
Judy <judy.mtz1@gmail.com>
2022-04-23 03:00:16 -0500

    added schema

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL 
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+);
